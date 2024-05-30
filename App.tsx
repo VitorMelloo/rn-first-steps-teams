cf487159f840a3f296b449e27a724a48a5594897
Vítor Mello <mello15.vitor@gmail.com>
2024-05-29 21:30:47 -0300

    :sparkles: feat: Adding a loading component

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
 import { ThemeProvider } from 'styled-components';
-import { ActivityIndicator } from 'react-native';
 import { Groups } from '@screens/groups';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import theme from '@theme/index'
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
